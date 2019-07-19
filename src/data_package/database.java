502009552cf43d1b24cd7599005e863a2a21ae9e
ChrisRolen <51124234+ChrisRolen@users.noreply.github.com>
2019-07-19 16:03:35 +0200

    Add files via upload

---
+++ src/data_package/database.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data_package;
+import java.sql.*;
+/**
+ *
+ * @author matteo
+ */
+public  class database {
+    public static Connection conn;
+    //Statement stmt;
+    //ResultSet rs;
+    //PreparedStatement pstmt;
+    public static Connection  connection() throws SQLException {
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/database_progetto?" + "user=root&password=cibermatteo98");
+        return conn;
+    }
+}
